8d76acbcaa694a2e0494c077abff6f0d967a66fc
Hector-Perez-Vengoa <hector.perez@tecsup.edu.pe>
2025-11-15 20:55:14 -0500

    Despliegue

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-
-  images: {
-    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+const nextConfig = {
+  turbopack: {
+    root: __dirname,
   },
-
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-export {default} from "next-auth/middleware";
+export { default as middleware } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/dashboard", "/profile"],
-}
+};

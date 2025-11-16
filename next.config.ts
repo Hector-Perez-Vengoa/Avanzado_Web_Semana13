fd4a1549a3022c854421b9d50a49dc38ed9c0746
Hector-Perez-Vengoa <hector.perez@tecsup.edu.pe>
2025-11-15 21:09:37 -0500

    imagenes

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
     ],
   },
+  /* config options here */
 };
 
 export default nextConfig;
